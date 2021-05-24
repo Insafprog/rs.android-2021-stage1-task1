eae1735c5f4145f53ddfedbebf1e82ec31c9bed9
Insafproger <BaKiR007>
2021-05-24 22:20:03 +0300

    Task finished

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,21 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        // check for empty array
+        if (sadArray.isEmpty())
+            return sadArray
+
+        val result = mutableListOf(sadArray.first())
+        for (i in 1..sadArray.size - 2) {
+            if (sadArray[i-1]+sadArray[i+1] >= sadArray[i])
+                result.add(sadArray[i])
+        }
+        result.add(sadArray.last())
+
+        // check again
+        if (sadArray.size != result.size)
+            return convertToHappy(result.toIntArray())
+        return result.toIntArray()
     }
 }
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,38 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+
+        fun parseString(input:String, regex: String): List<String>{
+            val rgx = Regex(regex)
+            val sequenceResult = rgx.findAll(input)
+            val result = mutableListOf<String>()
+            for (matchResult in sequenceResult) {
+                result.add(matchResult.value)
+                if (rgx.containsMatchIn(matchResult.value)) {
+                    result.addAll(parseString(matchResult.value, regex))
+                }
+            }
+            return result
+        }
+
+        val result = parseString(inputString,"""(?<=\[)([^\[]*?|.*?(\[.*?])+.
+            |*?)(?=])|(?<=<)([^<]*?|.*?(<.*?>)+.*?)(?=>)|(?<=\()([^(]*?|.*?
+            |(\(.*?\))+.*?)(?=\))""".trimMargin()).toMutableList()
+
+        // function find other valid text
+
+        fun otherResult(regex: String) {
+            for (str in parseString(inputString, regex)) {
+                if (str !in result)
+                    result.add(str)
+            }
+        }
+
+        otherResult("""(?<=\[)([^\[]*?|.*?(\[.*?])+.*?)(?=])""")
+        otherResult("""(?<=<)([^<]*?|.*?(<.*?>)+.*?)(?=>)""")
+        otherResult("""(?<=\()([^(]*?|.*?(\(.*?\))+.*?)(?=\))""")
+
+        return result.toTypedArray()
     }
 }
